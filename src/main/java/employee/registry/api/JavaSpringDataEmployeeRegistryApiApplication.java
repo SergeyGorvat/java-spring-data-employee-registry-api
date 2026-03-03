c8791c26d2d01d342afb26b66d7163b900edd908
SergeiGorvat <gorvat2012@yandex.ru>
2026-03-03 14:44:28 +0300

    Создал проект для подсчета сотрудников в компании.

---
+++ src/main/java/employee/registry/api/JavaSpringDataEmployeeRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package employee.registry.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDataEmployeeRegistryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDataEmployeeRegistryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/employee/registry/api/JavaSpringDataEmployeeRegistryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package employee.registry.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDataEmployeeRegistryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
